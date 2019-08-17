83b049c7f900d90e4970ff9ad079fbff18b70e61
My Name <my@mail.com>
2019-08-17 11:09:40 +0300

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,14 +23,22 @@ class Home extends Component{
         this.state.towns.map(town => fetch(`https://api.weatherbit.io/v2.0/current?city=${town}&key=ced73555abfb464ebcb5d7e77f5be270`)
         .then(res => res.json())
         .then(json => this.setState({...this.state,
-                                    townslist: townslist.push(json.data[0])}))
+                                    this.state.townslist: townslist.push(json.data[0])}))
             )
     }
 
     render() {
         console.log(this.state.townslist)
 
-        // Line 26:  'townslist' is not defined  no-undef
+    //     Line 26:  Parsing error: Unexpected keyword 'this'
+
+    //     24 |         .then(res => res.json())
+    //     25 |         .then(json => this.setState({...this.state,
+    //   > 26 |                                     this.state.townslist: townslist.push(json.data[0])}))
+    //        |                                     ^
+    //     27 |             )
+    //     28 |     }
+    //     29 | 
 
         return(
                 <div className="page">
