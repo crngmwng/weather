ffae59907c2d78ca46575128cab55f8bef47ad9c
My Name <my@mail.com>
2019-08-17 11:41:33 +0300

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ class Home extends Component{
         super(props)
         this.state = {
             towns: ["Sudak", "Yalta", "Kerch", "Feodosiya"],
-            townslist: []
+            townslist: {}
         }
     }
 
@@ -23,14 +23,20 @@ class Home extends Component{
         this.state.towns.map(town => fetch(`https://api.weatherbit.io/v2.0/current?city=${town}&key=ced73555abfb464ebcb5d7e77f5be270`)
         .then(res => res.json())
         .then(json => this.setState({...this.state,
-                                    townslist: this.state.townslist.push(json.data[0])}))
+                                    townslist: {...this.state.townslist, ...json.data[0]}
+                                })
             )
+        )
     }
 
     render() {
         console.log(this.state.townslist)
-        
-        // Unhandled Rejection (TypeError): this.state.townslist.push is not a function
+
+        // {}
+        // {rh: 38, pod: "d", lon: 34.97471, pres: 1010.24, timezone: "Europe/Simferopol", …}
+        // {rh: 60, pod: "d", lon: 36.47429, pres: 1004.72, timezone: "Europe/Simferopol", …}
+        // {rh: 63, pod: "d", lon: 37.27365, pres: 1007.1, timezone: "Europe/Kiev", …}
+        // {rh: 45, pod: "d", lon: 35.37789, pres: 1001.59, timezone: "Europe/Simferopol", …}
 
         return(
                 <div className="page">
