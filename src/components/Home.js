305673bea7c614a772af4dff40ba2109d4b43dc2
My Name <my@mail.com>
2019-08-17 10:44:05 +0300

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,24 +23,25 @@ class Home extends Component{
         this.state.towns.map(town => fetch(`https://api.weatherbit.io/v2.0/current?city=${town}&key=ced73555abfb464ebcb5d7e77f5be270`)
         .then(res => res.json())
         .then(json => this.setState({...this.state,
-                                    townslist: json.data[0]})
+                                    townslist.push(json.data[0]}))
             )
         )
     }
 
     render() {
         console.log(this.state.townslist)
-
-        // []
-        // length: 0
-        // __proto__: Array(0)
-        
-        // {rh: 42, pod: "d", lon: 34.97471, pres: 1010.51, timezone: "Europe/Simferopol", …}
-        // {rh: 67, pod: "d", lon: 36.47429, pres: 1004.75, timezone: "Europe/Simferopol", …}
-        // {rh: 70, pod: "d", lon: 37.27365, pres: 1007.41, timezone: "Europe/Kiev", …}
-        // {rh: 48, pod: "d", lon: 35.37789, pres: 1001.88, timezone: "Europe/Simferopol", …}
-
-
+// 
+        // Line 26:  Parsing error: Unexpected token, expected ","
+// 
+        // 24 |         .then(res => res.json())
+        // 25 |         .then(json => this.setState({...this.state,
+    //   > 26 |                                     townslist.push(json.data[0]}))
+        //    |                                              ^
+        // 27 |             )
+        // 28 |         )
+        // 29 |     }
+// 
+// 
         return(
                 <div className="page">
                     <Navbar /> 
